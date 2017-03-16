6bfc450edb977329296d3106df09f58039bc49a7
vuru <suleyman.vurucu@sms-lsn.com>
2017-03-16 16:12:39 +0100

    Register with IP hack

--- springboot/cloud-enterprise-platform/cloud-enterprise-platform-configuration/src/main/java/eu/amova/cloud/platform/service/configuration/CloudEnterprisePlatformConfigurationApplication.java
+++ springboot/cloud-enterprise-platform/cloud-enterprise-platform-configuration/src/main/java/eu/amova/cloud/platform/service/configuration/CloudEnterprisePlatformConfigurationApplication.java
@@ -1,10 +1,14 @@
 package eu.amova.cloud.platform.service.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 @EnableAutoConfiguration
@@ -12,8 +16,21 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigServer
 public class CloudEnterprisePlatformConfigurationApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(CloudEnterprisePlatformConfigurationApplication.class, args);
+	}
+
+	@Autowired
+	private EurekaInstanceConfigBean conf;
+	@PostConstruct
+	private void init( ){
+		if(conf.isPreferIpAddress()) {
+			// worarround for get the hosname instaed of ip adress here.
+			conf.setPreferIpAddress(false);
+			String currentInstanceId = conf.getInstanceId();
+			conf.setInstanceId(currentInstanceId.replace(conf.getHostname(),conf.getIpAddress()));
+			conf.setPreferIpAddress(true);
+		}
+		
 	}
 }
