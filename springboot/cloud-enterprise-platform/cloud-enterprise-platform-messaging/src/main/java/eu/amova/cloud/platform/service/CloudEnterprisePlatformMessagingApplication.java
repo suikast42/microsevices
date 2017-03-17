5803892f11da3fcee689fb0a37a3434ac9b51288
vuru <suleyman.vurucu@sms-lsn.com>
2017-03-17 10:05:11 +0100

    messaging

---
+++ springboot/cloud-enterprise-platform/cloud-enterprise-platform-messaging/src/main/java/eu/amova/cloud/platform/service/CloudEnterprisePlatformMessagingApplication.java
@@ -0,0 +1,14 @@
+package eu.amova.cloud.platform.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEnterprisePlatformMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEnterprisePlatformMessagingApplication.class, args);
+	}
+
+
+}
